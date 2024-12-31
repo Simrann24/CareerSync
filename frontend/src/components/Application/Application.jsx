e82edece996a04f99e40c2c5dd09f1eefd44ac5a
Simrandeep Singh <160052559+Simrann24@users.noreply.github.com>
2024-12-31 20:04:08 +0530

    Update Application.jsx

--- frontend/src/components/Application/Application.jsx
+++ frontend/src/components/Application/Application.jsx
@@ -31,7 +31,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/application/post",
+        "https://careersync-frontend.onrender.com/api/v1/application/post",
         formData,
         {
           withCredentials: true,
