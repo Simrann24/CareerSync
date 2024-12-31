b03c4901e2a9ccc14db19595ee23cee2e5900a07
Simrandeep Singh <160052559+Simrann24@users.noreply.github.com>
2025-01-01 00:18:53 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,52 +1,16 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-
-// User Schema definition
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false, // Do not return password by default in queries
-  },
-  role: {
-    type: String,
-    default: 'Job Seeker', // Default role is Job Seeker
-  },
-});
-
-// Pre-save hook to hash password
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-// Method to generate JWT Token
-userSchema.methods.getJWTToken = function() {
-  return jwt.sign(
-    { id: this._id, role: this.role }, // Payload: user id and role
-    process.env.JWT_SECRET_KEY, // Secret key for signing JWT
-    {
-      expiresIn: process.env.JWT_EXPIRE || '1d', // JWT expiry time (default: 1 day)
-    }
-  );
+export const sendToken=(user,statusCode,res,message)=>{
+    const token=user.getJWTToken();
+    const options={
+        expires:new Date(
+            Date.now()+process.env.COOKIE_EXPIRE*24*60*60*1000
+        ),
+        httpOnly:true,
+    };
+    res.status(statusCode).cookie("token",token,options).json({
+        success:true,
+        user,
+        message,
+        token,
+    });
 };
-
-// Method to compare passwords (for login)
-userSchema.methods.comparePassword = async function(enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-// Export the User model
-export const User = mongoose.model('User', userSchema);
 
