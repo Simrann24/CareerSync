064fb40ac1e3bd88c424524b09c51d5d54fce0cb
Simrandeep Singh <160052559+Simrann24@users.noreply.github.com>
2024-12-31 20:33:02 +0530

    Update MyApplication.jsx

--- frontend/src/components/Application/MyApplication.jsx
+++ frontend/src/components/Application/MyApplication.jsx
@@ -18,7 +18,7 @@ const MyApplications = () => {
     try {
       if (user && user.role === "Employer") {
         axios
-          .get("https://careersync-frontend.onrender.com/api/v1/application/employer/getall", {
+          .get("https://careersync-backend.onrender.com/api/v1/application/employer/getall", {
             withCredentials: true,
           })
           .then((res) => {
@@ -26,7 +26,7 @@ const MyApplications = () => {
           });
       } else {
         axios
-          .get("https://careersync-frontend.onrender.com/api/v1/application/jobseeker/getall", {
+          .get("https://careersync-backend.onrender.com/api/v1/application/jobseeker/getall", {
             withCredentials: true,
           })
           .then((res) => {
@@ -45,7 +45,7 @@ const MyApplications = () => {
   const deleteApplication = (id) => {
     try {
       axios
-        .delete(`https://careersync-frontend.onrender.com/api/v1/application/delete/${id}`, {
+        .delete(`https://careersync-backend.onrender.com/api/v1/application/delete/${id}`, {
           withCredentials: true,
         })
         .then((res) => {
