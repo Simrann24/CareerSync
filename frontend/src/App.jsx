3f801ce76cdf80d033d41d59d7b31c7bfd7aa907
Simrandeep Singh <160052559+Simrann24@users.noreply.github.com>
2024-12-31 23:39:53 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import React ,{useEffect,useContext}  from 'react'
+import React, { useEffect, useContext } from "react";
 import "./App.css";
-import {Context} from "./main";
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import { Context } from "./main";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from './components/Auth/login';
 import Register from './components/Auth/Register';
 import Navbar from './components/Layout/Navbar';
@@ -12,50 +12,60 @@ import JobDetails from './components/Job/JobDetail';
 import MyJobs from './components/Job/MyJobs';
 import PostJobs from './components/Job/PostJobs';
 import Application from './components/Application/Application';
-import MyApplication from './components/Application/MyApplication'
+import MyApplication from './components/Application/MyApplication';
 import NotFound from './components/NotFound/NotFound.jsx';
 import axios from 'axios';
-import {Toaster} from 'react-hot-toast';
-
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-  const {isAuthorized,setIsAuthorized,setUser}=useContext(Context);
+  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
 
-  useEffect(()=>{  
-    const fetchUser=async()=>{
-      try{
-        const response=await axios.get("https://careersync-backend.onrender.com/api/v1/user/getuser",{withCredentials:true});
-        setUser((response.data.user));
-      }
-      catch(error){
-        setIsAuthorized(false);
+  useEffect(() => {
+    // This function will run only once when the component mounts
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get("https://careersync-backend.onrender.com/api/v1/user/getuser", {
+          withCredentials: true, // Make sure cookies are sent with the request
+        });
+        
+        // If the response has user data, update the context
+        if (response.data && response.data.user) {
+          setUser(response.data.user);
+          setIsAuthorized(true); // Mark as authorized if user data is fetched
+        } else {
+          setIsAuthorized(false); // Mark as not authorized if no user is returned
+        }
+      } catch (error) {
+        setIsAuthorized(false); // If there's an error, mark as not authorized
       }
     };
+
+    // Run fetchUser function
     fetchUser();
-  },[isAuthorized]);
-  
+  }, [setIsAuthorized, setUser]); // This will run only once after the component mounts
 
   return (
     <>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/job/getall" element={<Jobs/>}/>
-          <Route path="/job/:id" element={<JobDetails/>}/>
-          <Route path="/job/post" element={<PostJobs/>}/>
-          <Route path="/job/me" element={<MyJobs/>}/>
-          <Route path="/application/:id" element={<Application/>}/>
-          <Route path="/applications/me" element={<MyApplication/>}/>
-          <Route path="*" element={<NotFound/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/job/getall" element={<Jobs />} />
+          <Route path="/job/:id" element={<JobDetails />} />
+          <Route path="/job/post" element={<PostJobs />} />
+          <Route path="/job/me" element={<MyJobs />} />
+          <Route path="/application/:id" element={<Application />} />
+          <Route path="/applications/me" element={<MyApplication />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-        <Footer/>
-        <Toaster/>
+        <Footer />
+        <Toaster />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
