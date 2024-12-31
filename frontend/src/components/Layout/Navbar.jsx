c78776dd6967e21fdd45ec158dff16d8d9c52d44
Simrandeep Singh <160052559+Simrann24@users.noreply.github.com>
2024-12-31 20:47:23 +0530

    Update Navbar.jsx

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const {isAuthorized,setIsAuthorized,user}=useContext(Context);
   const handleLogout = async () => {
     try {
-      const response = await axios.get("http://localhost:4000/api/v1/user/logout",{withCredentials: true,});
+      const response = await axios.get("https://careersync-backend.onrender.com/api/v1/user/logout",{withCredentials: true,});
       toast.success(response.data.message);
       setIsAuthorized(false);
       navigateTo("/login");
