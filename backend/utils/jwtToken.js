1a6834494cf12b1772c5f542201d43ba7eb66dba
Simrandeep Singh <160052559+Simrann24@users.noreply.github.com>
2025-01-01 00:29:50 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,19 +1,16 @@
 export const sendToken = (user, statusCode, res, message) => {
-  const token = user.getJWTToken(); // Generate JWT Token
+    const token = user.getJWTToken();
+    const options = {
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true, // Prevent XSS attacks
+        secure: process.env.NODE_ENV === 'production', // Secure cookies in production
+        sameSite: 'Lax', // Ensure CSRF protection
+    };
 
-  const options = {
-    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000), // Token expiration
-    httpOnly: false, // Allows JavaScript to access the cookie (use false for testing purposes)
-    secure: process.env.NODE_ENV === 'production', // Only set 'secure' flag in production (for HTTPS)
-    sameSite: 'strict', // Protects from CSRF
-  };
-
-  // Send response with token as cookie
-  res.status(statusCode).cookie('token', token, options).json({
-    success: true,
-    user,
-    message,
-    token, // The token is also included in the response body
-  });
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        user,
+        message,
+        token,
+    });
 };
-
