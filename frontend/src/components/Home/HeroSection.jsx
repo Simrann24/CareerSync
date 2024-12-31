aefb5c0bfaf03e8126f51090bfa87a3c562cb63b
Kendo007 <110466301+Kendo007@users.noreply.github.com>
2024-12-31 23:41:11 +0530

    Updatef some data

--- frontend/src/components/Home/HeroSection.jsx
+++ frontend/src/components/Home/HeroSection.jsx
@@ -36,9 +36,7 @@ const HeroSection = () => {
             <h1>Find a job that suits</h1>
             <h1>your interests and skills</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              voluptate repellat modi quidem aliquid eaque ducimus ipsa et,
-              facere mollitia!
+            Whether you're exploring new opportunities or ready to take the next big step in your career, we provide a platform that aligns your skills and interests with the perfect job. Start achieving your professional dreams today
             </p>
           </div>
           <div className="image">
